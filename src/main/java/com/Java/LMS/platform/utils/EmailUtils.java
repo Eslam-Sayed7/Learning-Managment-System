cd3007501acfea5f6639d0ff599a40a43ffbe571
Eslam-Sayed7 <eslam.sayed.ed@gmail.com>
2024-12-22 13:37:28 +0200

    REFACT:generic send mail function

--- src/main/java/com/Java/LMS/platform/utils/EmailUtils.java
+++ src/main/java/com/Java/LMS/platform/utils/EmailUtils.java
@@ -1,7 +1,27 @@
 package com.Java.LMS.platform.utils;
 
+import com.Java.LMS.platform.service.dto.Email.EmailFormateDto;
+
 public class EmailUtils {
     public static String getEmailMessage(String name){
         return "Salam " + name + ", \n\nYour new account has been created.";
     }
+    public static EmailFormateDto registerationEmail( String to) {
+        EmailFormateDto emailFormateDto = new EmailFormateDto();
+        emailFormateDto.setTo(to);
+        emailFormateDto.setSubject("New User Account Verification");
+        emailFormateDto.setEmailBody("Welcome to the LMS as new User");
+
+        return emailFormateDto;
+    }
+    public static EmailFormateDto CourseActionEmail( String to) {
+        EmailFormateDto emailFormateDto = new EmailFormateDto();
+        emailFormateDto.setTo(to);
+        emailFormateDto.setSubject("New User Account Verification");
+        emailFormateDto.setEmailBody("Welcome to the LMS as new User");
+
+        return emailFormateDto;
+    }
+
+
 }
